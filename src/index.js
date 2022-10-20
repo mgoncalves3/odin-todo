b6827d0c66caa7b027fd3901a0932ce2f180448d
mgoncalves3 <marco.g@sapo.pt>
2022-10-20 18:45:47 +0100

    ui changes

--- src/index.js
+++ src/index.js
@@ -19,3 +19,30 @@ checkBtns.forEach(btn => {
     btn.classList.toggle('bi-circle');
   })
 })
+
+// const textArea = document.getElementsByTagName("textarea");
+// for (let i = 0; i < textArea.length; i++) {
+//   textArea[i].setAttribute("style", "overflow-y:hidden;");
+//   textArea[i].style.height = textArea[i].scrollHeight;
+//   textArea[i].addEventListener("input", OnInput, false);
+// }
+
+// function OnInput() {
+//   this.style.height = 0;
+//   this.style.height = (this.scrollHeight) + "px";
+// }
+
+
+const tx = document.getElementsByTagName("textarea");
+for (let i = 0; i < tx.length; i++) {
+  tx[i].setAttribute("style", "height:" + (tx[i].scrollHeight) + "px;overflow-y:hidden;");
+  tx[i].addEventListener("input", OnInput, false);
+}
+
+function OnInput() {
+  this.style.height = 0;
+  this.style.height = (this.scrollHeight) + "px";
+  console.log("i ran")
+}
+
+console.log(tx);
