2e21096a779a9b3a923602c009e9537ba389951d
Marco Gonçalves <55608718+mgoncalves3@users.noreply.github.com>
2022-11-14 00:11:42 +0000

    start

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
-# odin-todo
-The Odin Project Todo list app
+# Todo List
+## [The Odin Project](https://www.theodinproject.com/lessons/node-path-javascript-todo-list)
+---
+### App needs
+- Use webpack
+- Have default page with every task
+- Separate 'folders' for projects
+- Be able to expand tasks for more details or editing
+- Delete tasks
+- Use localStorage for data persistence
+- Retrieve data from earlier sessions
+
+### Tasks must have
+- Title
+- Description
+- Due date
+- Priority
+---
+### I'll try
+- Use date-fns library
+- Not use CSS library
+- Follow [Material UI 3](https://m3.material.io/) design language and [icons](https://fonts.google.com/icons)
--- webpack.config.js
+++ webpack.config.js
@@ -1,66 +1,12 @@
-const path = require("path");
-const webpack = require("webpack");
-const WebpackDevServer = require("webpack-dev-server");
-const miniCssExtractPlugin = require("mini-css-extract-plugin");
+const path = require('path');
 
 module.exports = {
-  mode: "development",
-  entry: {
-    main: path.resolve(__dirname, "./src/index.js"),
-  },
+  entry: "~/src/app.js",
   output: {
     path: path.resolve(__dirname, "dist"),
-    filename: "main.bundle.js",
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new miniCssExtractPlugin(),
-  ],
-  devServer: {
-    static: {
-      directory: path.join(__dirname, "./dist"),
-    },
-    historyApiFallback: true,
-    open: true,
-    compress: true,
-    hot: true,
-    port: 8080,
+    filename: "bundle.js",
   },
   module: {
-    rules: [
-      // Javascript
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ["babel-loader"],
-      },
-      {
-        test: /\.(?:ico|gif|png|jpg|jpeg)$/i,
-        type: "asset/resource",
-      },
-      {
-        test: /\.(woff(2)?|eot|ttf|otf|svg|)$/,
-        type: "asset/inline",
-      },
-      {
-        test: /\.(scss|css)$/,
-        use: [
-          // 'style-loader' bundles CSS together with the main app bundle
-          // That may cause performance problems when the bundle get's big
-          // { loader: "style-loader" },
-
-          // Adds CSS to the DOM by injecting a `<style>` tag in the html
-          { loader: miniCssExtractPlugin.loader },
-          { loader: "css-loader" },
-          {
-            loader: "postcss-loader",
-            options: {
-              postcssOptions: { plugins: () => [require("autoprefixer")] },
-            },
-          },
-          { loader: "sass-loader" },
-        ],
-      },
-    ],
-  },
+    rules: [{ test: /\.txt$/, use: 'raw-loader'}],
+  }
 };
