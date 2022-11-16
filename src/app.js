5240ba44704c0178bfd81c970b7b0781ba3bba5b
Marco Gonçalves <55608718+mgoncalves3@users.noreply.github.com>
2022-11-16 19:56:30 +0000

    add task modal added

--- src/app.js
+++ src/app.js
@@ -1,3 +1,30 @@
 import "./styles.css";
 import "./styles.scss";
-import 'material-icons/css/material-icons.css';
+import "material-icons/css/material-icons.css";
+
+// Animate list item dropdown menu
+const dropdownShowBtn = document.querySelector(".more-options-btn");
+const dropdownMenu = document.querySelector(".dropdown");
+
+dropdownShowBtn.addEventListener("click", () => {
+  dropdownMenu.classList.toggle("visible");
+});
+
+// Open/Close add task modal
+const modal = document.getElementById("modal-background");
+const addTaskBtn = document.querySelector(".new-todo-btn-open-modal");
+const modalCloseBtn = document.getElementById("modal-close-btn");
+
+addTaskBtn.addEventListener("click", () => {
+  modal.style.display = "flex";
+  modal.setAttribute("data-openstatus", "open");
+});
+
+window.addEventListener("click", (e) => {
+  if (e.target == modal || e.target == modalCloseBtn) {
+    if (modal.dataset.openstatus === "open") {
+      modal.style.display = "none";
+      modal.setAttribute("data-openstatus", "closed");
+    }
+  }
+});
